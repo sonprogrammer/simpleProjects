2c4c89fb828b5b4e1a275ce9ba2566c44058e6b0
sonprogrammer <ods04138@gmail.com>
2024-01-30 20:31:34 +0900

    Feat : Header setting

---
+++ starbucks/main.js
@@ -0,0 +1,16 @@
+const searchEl = document.querySelector('.search')
+const searchInput = searchEl.querySelector('input')
+
+searchEl.addEventListener('click', () =>{
+    searchInput.focus();
+})
+
+searchInput.addEventListener('focus', () =>{
+    searchEl.classList.add('focused')
+    searchInput.setAttribute('placeholder', '통합검색');
+})
+
+searchInput.addEventListener('blur', () =>{
+    searchEl.classList.remove('focused')
+    searchInput.setAttribute('placeholder', '');
+})
