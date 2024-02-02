975298bcb364d6f646250f4e971ac624f006c3c0
sonprogrammer <ods04138@gmail.com>
2024-02-02 19:18:15 +0900

    Feat : auto swiper  notice

--- starbucks/main.js
+++ starbucks/main.js
@@ -43,3 +43,11 @@ fadeEls.forEach((fadeEl, index)=>{
         opacity: 1,
     })
 })
+
+
+//* new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container',{
+    direction: 'vertical',
+    autoplay: true,
+    loop: true,
+});
