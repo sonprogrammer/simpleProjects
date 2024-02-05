ac3c2fe6b7dfce504a9045cdf77ae24ea93047cd
sonprogrammer <ods04138@gmail.com>
2024-02-05 20:01:38 +0900

    Feating : animation effect

--- starbucks/main.js
+++ starbucks/main.js
@@ -103,3 +103,16 @@ function floatingObj(selector, delay, size) {
 floatingObj('.floating1', 1, 15)
 floatingObj('.floating2', 0.5, 10)
 floatingObj('.floating3', 1.5, 20)
+
+
+
+const spyEls = document.querySelectorAll('.section.scroll-spy');
+spyEls.forEach((spyEl) => {
+    new ScrollMagic
+        .Scene({
+            triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+            triggerHook: 0.8
+        })
+        .setClassToggle(spyEl, 'show')
+        .addTo(new ScrollMagic.Controller());
+});
