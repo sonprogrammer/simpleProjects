2a2736296d7c73b3564aa83e4fb6b01f186bf7c1
sonprogrammer <ods04138@gmail.com>
2024-02-08 17:22:07 +0900

    Feat : search bar

--- ipad/js/main.js
+++ ipad/js/main.js
@@ -1,3 +1,4 @@
+//* 장바구니
 const basketStarterEl = document.querySelector('header .basket-starter');
 const basketEl = basketStarterEl.querySelector('.basket');
 
@@ -26,4 +27,36 @@ function showBasket(){
 }
 function hideBasket(){
     basketEl.classList.remove('show')
+}
+
+
+// *검색
+const headerEl = document.querySelector('header');
+const searchWrapEl = headerEl.querySelector('.search-wrap');
+const searchStarterEl = headerEl.querySelector('.search-starter');
+const searchCloserEl = headerEl.querySelector('.search-closer');
+const searchShadowEl = headerEl.querySelector('.shadow');
+
+searchStarterEl.addEventListener('click', ()=>{
+    showSearch()
+})
+
+searchCloserEl.addEventListener('click', ()=>{
+    hideSearch();
+})
+
+searchShadowEl.addEventListener('click', ()=>{
+    hideSearch()
+})
+
+
+
+function showSearch(){
+    headerEl.classList.add('searching')
+    document.documentElement.classList.add('fixed')
+}
+
+function hideSearch(){
+    headerEl.classList.remove('searching')
+    document.documentElement.classList.remove('fixed')
 }
