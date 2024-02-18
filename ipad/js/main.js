505e52cc8f174b814871337e36d0d1eb0f57f70e
sonprogrammer <ods04138@gmail.com>
2024-02-18 20:07:41 +0900

    Feat : play Video

--- ipad/js/main.js
+++ ipad/js/main.js
@@ -95,3 +95,20 @@ const infoEls = document.querySelectorAll('.info');
 infoEls.forEach((el)=>{
     io.observe(el)
 })
+
+
+// ! 비디오 재생
+const video = document.querySelector('.stage video');
+const playBtn = document.querySelector('.stage .controller--play');
+const pauseBtn = document.querySelector('.stage .controller--pause');
+
+playBtn.addEventListener('click', () =>{
+    video.play()
+    playBtn.classList.add('hide');
+    pauseBtn.classList.remove('hide');
+})
+pauseBtn.addEventListener('click', () =>{
+    video.pause()
+    playBtn.classList.remove('hide');
+    pauseBtn.classList.add('hide');
+})
