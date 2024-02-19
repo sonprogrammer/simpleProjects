4d6aa0d1970a5600457e9b8c23fa23e15194fd8c
sonprogrammer <ods04138@gmail.com>
2024-02-19 18:42:56 +0900

    Feat : footer navigation

--- ipad/js/main.js
+++ ipad/js/main.js
@@ -1,4 +1,5 @@
 import ipads from '../data/ipads.js';
+import navigations from '../data/navigations.js';
 
 
 
@@ -147,4 +148,29 @@ ipads.forEach((ipad)=>{
     `
 
     itemsEl.append(itemEl)
+})
+
+
+const navigationsEl = document.querySelector('footer .navigations')
+
+navigations.forEach((nav)=>{
+    const mapEl = document.createElement('div')
+    mapEl.classList.add('map')
+
+    let mapList = ''
+    nav.maps.forEach((map)=>{
+        mapList += /*html*/ `<li>
+        <a href="${map.url}">${map.name}</a>
+        </li>`
+    })
+    
+    mapEl.innerHTML = /*html*/ `
+        <h3>
+            <span class="text">${nav.title}</span>
+        </h3>
+        <ul>
+            ${mapList}
+        </ul>
+    `
+    navigationsEl.append(mapEl)
 })
