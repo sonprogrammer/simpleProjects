b88a6d7f2792f01fa87dfe11b7b458d089834dac
sonprogrammer <ods04138@gmail.com>
2024-02-19 16:54:45 +0900

    Feat : compare ipad models

--- ipad/js/main.js
+++ ipad/js/main.js
@@ -1,3 +1,8 @@
+import ipads from '../data/ipads.js';
+
+
+
+
 //* 장바구니
 const basketStarterEl = document.querySelector('header .basket-starter');
 const basketEl = basketStarterEl.querySelector('.basket');
@@ -111,4 +116,35 @@ pauseBtn.addEventListener('click', () =>{
     video.pause()
     playBtn.classList.remove('hide');
     pauseBtn.classList.add('hide');
+})
+
+
+
+
+// !ipad 렌더링
+const itemsEl = document.querySelector('section.compare .items')
+ipads.forEach((ipad)=>{
+    const itemEl = document.createElement('div');
+    itemEl.classList.add('item');
+
+    let colorList = ''
+    ipad.colors.forEach((color)=>{
+        colorList += `<li style="background-color: ${color};"></li>`
+    })
+    
+    itemEl.innerHTML =  /* html */ `
+    <div class="thumbnail">
+        <img src="${ipad.thumbnail}" alt="${ipad.name}" />
+    </div>
+    <ul class="colors">
+        ${colorList}
+    </ul>
+    <h3 class="name">${ipad.name}</h3>
+    <p class="tagline">${ipad.tagline}</p>
+    <p class="price"> ￦${ipad.price.toLocaleString('en-us')}부터</p>
+    <button class="btn">구입하기</button>
+    <a href="${ipad.url}" class="link">더 알아보기</a>
+    `
+
+    itemsEl.append(itemEl)
 })
