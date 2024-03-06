bc6c06781973e00af289b36978ccf965cf0415da
sonprogrammer <ods04138@gmail.com>
2024-03-06 19:53:14 +0900

    ts

--- movie_web/src/core/heropy.ts
+++ movie_web/src/core/heropy.ts
@@ -1,3 +1,4 @@
+
 //*Component 
 interface ComponentPayload{
   tagName?: string
@@ -32,7 +33,14 @@ export class Component {
   
   //*Router
   // 페이지 렌더링!
-  function routeRender(routes) {
+  interface Route{
+    path: string
+    component: typeof Component
+  }
+
+  type Routes = Route[]
+
+  function routeRender(routes: Routes) {
     // 접속할 때 해시 모드가 아니면(해시가 없으면) /#/로 리다이렉트!
     if (!location.hash) {
       history.replaceState(null, '', '/#/') // (상태, 제목, 주소)
@@ -41,24 +49,32 @@ export class Component {
     const [hash, queryString = ''] = location.hash.split('?') // 물음표를 기준으로 해시 정보와 쿼리스트링을 구분
   
     // 1) 쿼리스트링을 객체로 변환해 히스토리의 상태에 저장!
+    interface Query {
+      [key: string]: string
+    }
+    
     const query = queryString
       .split('&')
       .reduce((acc, cur) => {
         const [key, value] = cur.split('=')
         acc[key] = value
         return acc
-      }, {})
+      }, {} as Query)
     history.replaceState(query, '') // (상태, 제목)
   
     // 2) 현재 라우트 정보를 찾아서 렌더링!
     const currentRoute = routes.find(route => new RegExp(`${route.path}/?$`).test(hash))
-    routerView.innerHTML = ''
-    routerView.append(new currentRoute.component().el)
+
+    if(routerView){
+      routerView.innerHTML = ''
+      currentRoute && routerView.append(new currentRoute.component().el)
+    }
+
   
     // 3) 화면 출력 후 스크롤 위치 복구!
     window.scrollTo(0, 0)
   }
-  export function createRouter(routes) {
+  export function createRouter(routes: Routes) {
     // 원하는(필요한) 곳에서 호출할 수 있도록 함수 데이터를 반환!
     return function () {
       window.addEventListener('popstate', () => {
