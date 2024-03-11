2bc58c5f1d253a0c1b91d84b4eb1fa1c6e7d5d00
sonprogrammer <ods04138@gmail.com>
2024-03-11 19:53:19 +0900

    convert to typescript, using transforms tools

--- movie_web/src/store/about.ts
+++ movie_web/src/store/about.ts
@@ -1,6 +1,15 @@
 import { Store } from "../core/heropy";
 
-export default new Store({
+
+interface State{
+    photo: string
+    name: string
+    email: string
+    github: string
+    repository: string
+}
+
+export default new Store<State>({
     photo: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2F20140111_39%2Fconselot_13894439521335n6j2_JPEG%2Fimg13.jpg&type=sc960_832',
     name: 'youngin son',
     email: 'ods04138@gmail.com',
