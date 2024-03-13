5ba4e123c16d628c631632f8842d8d55d9c4bc09
sonprogrammer <ods04138@gmail.com>
2024-03-13 20:38:37 +0900

    Feat: categroy components

--- disney-app/src/App.js
+++ disney-app/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Nav  from './components/Nav'
 import Banner from './components/Banner';
 import {Container} from './App-styles';
+import Category from './components/Category';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Container>
       <Nav />
       <Banner />
+      <Category />
     </Container>
   );
 }
--- disney-app/src/components/Category.js
+++ disney-app/src/components/Category.js
@@ -1,10 +1,40 @@
 import React from 'react'
+import { Container,Wrap } from './Category-styles'
 
 const Category = () => {
   return (
-    <div>
-      
-    </div>
+    <Container>
+      <Wrap>
+        <img src="/images/viewers-disney.png" alt="disney" />
+        <video autoPlay loop muted>
+          <source src='/videos/disney.mp4' type='video/mp4' />
+        </video>
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-marvel.png" alt="marvel" />
+        <video autoPlay loop muted>
+          <source src='/videos/marvel.mp4' type='video/mp4' />
+        </video>
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-national.png" alt="national-geographic" />
+        <video autoPlay loop muted>
+          <source src='/videos/national-geographic.mp4' type='video/mp4' />
+        </video>
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-pixar.png" alt="pixar" />
+        <video autoPlay loop muted>
+          <source src='/videos/pixar.mp4' type='video/mp4' />
+        </video>
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-starwars.png" alt="starwars" />
+        <video autoPlay loop muted>
+          <source src='/videos/star-wars.mp4' type='video/mp4' />
+        </video>
+      </Wrap>
+    </Container>
   )
 }
 
