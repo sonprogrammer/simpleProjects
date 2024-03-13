6702adfc9206e64a5e8fd3274626c029787725a0
sonprogrammer <ods04138@gmail.com>
2024-03-13 22:06:23 +0900

    Feat: slide function

--- disney-app/src/components/Row.js
+++ disney-app/src/components/Row.js
@@ -1,45 +1,47 @@
 import axios from '../api/axios'
 import React, { useCallback, useEffect, useState } from 'react'
+import './Row.css'
 
-function Row({title, id, fetchURL}) {
-
+function Row({ title, id, fetchURL }) {
   const [movies, setMovies] = useState([])
 
-  
-  const fetchMovieData = useCallback(async()=>{
+  const fetchMovieData = useCallback(async () => {
     const response = await axios.get(fetchURL)
     console.log(response)
     setMovies(response.data.results)
-  },[fetchURL])
-  
-  useEffect(()=>{
+  }, [fetchURL])
+
+  useEffect(() => {
     fetchMovieData()
-  },[fetchMovieData])
+  }, [fetchMovieData])
 
   return (
     <div>
       <h2>{title}</h2>
-      <div className="slider">
-        <div className="slider__arrow-left">
-          <span className="arrow">
-            {"<"}
-          </span>
+      <div className='slider'>
+        <div className='slider__arrow-left'>
+          <span className='arrow'
+          onClick={()=>{
+            document.getElementById(id).scrollLeft -= window.innerWidth - 80
+          }}
+          >{'<'}</span>
         </div>
-        <div id={id}className="row__posters">
-        {movies.map(movie => (
-  <img
-    key={movie.id}
-    className="row__poster"
-    src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
-    alt={movie.name}
-  />
-))}
-
+        <div id={id} className='row__posters'>
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              className='row__poster'
+              src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+              alt={movie.name}
+            />
+          ))}
         </div>
-        <div className="slieder__arrow-right">
-          <span className="arrow">
-            {">"}
-          </span>
+        <div className='slider__arrow-right'>
+          <span className='arrow'
+          onClick={()=>{
+            document.getElementById(id).scrollLeft += window.innerWidth - 80
+          }}
+          >{'>'}</span>
         </div>
       </div>
     </div>
