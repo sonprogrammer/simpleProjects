34b2578b71757f6c8f20275547fd6602afee25e4
sonprogrammer <ods04138@gmail.com>
2024-03-15 16:32:31 +0900

    Feat: search page UI

--- disney-app/src/pages/SearchPage/SearchPage.js
+++ disney-app/src/pages/SearchPage/SearchPage.js
@@ -1,9 +1,10 @@
 import axios from '../../api/axios'
 import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
+import './SearchPage.css'
 
 export default function SearchPage() {
-  const [seachResults, setSearchResults] = useState([])
+  const [searchResults, setSearchResults] = useState([])
 
   const useQuery = () =>{
     return new URLSearchParams(useLocation().search)
@@ -12,6 +13,7 @@ export default function SearchPage() {
   let query = useQuery()
   const searchTerm = query.get('q')
 
+  const navigate = useNavigate()
 
   useEffect(()=>{
     if(searchTerm){
@@ -22,16 +24,38 @@ export default function SearchPage() {
   const fetchSearchMovie = async () => {
     try{
       const response = await axios.get(`/search/multi?include_adult=false&query=${searchTerm}`);
-      setSearchResults(response.data.result)
+      setSearchResults(response.data.results)
       console.log(response.data)
     }catch(error){
       console.log(error)
     }
   }
 
-  return (
-    <div>
-      
-    </div>
-  )
+  if(searchResults.length > 0){
+    return(
+      <section className='search-container'>
+        {searchResults.map((movie) => {
+          if(movie.backdrop_path !== null && movie.media_type !== "person"){
+            const movieImageUrl = "https://image.tmdb.org/t/p/w500" + movie.backdrop_path
+            return(
+              <div className='movie' key={movie.id}>
+                <div className='movie__column-poster' onClick={()=>navigate(`/${movie.id}`)}>
+                  <img src={movieImageUrl} alt="movie" className='movie__poster' />
+                </div>
+              </div>
+            )
+          }
+        })}
+      </section>
+
+    )
+  }else{
+    return(
+      <section className='no-results'>
+        <div className='no-results__text'>
+          <p>찾고자하는 검색어 "{searchTerm}" 에 맞는 영화가 없습니다.</p>
+        </div>
+      </section>
+    )
+  }
 }
