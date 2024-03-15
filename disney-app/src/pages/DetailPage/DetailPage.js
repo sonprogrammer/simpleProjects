d8e803aea39c916dc54e32b61d6bb7675a0f99bd
sonprogrammer <ods04138@gmail.com>
2024-03-15 17:33:21 +0900

    Feat : detail page

--- disney-app/src/pages/DetailPage/DetailPage.js
+++ disney-app/src/pages/DetailPage/DetailPage.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import axios from '../../api/axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function DetailPage() {
+  let {movieId}= useParams()
+  const [movie, setMovie] = useState({})
+
+  useEffect(()=>{
+    async function fetchData(){
+      const response = await axios.get(`/movie/${movieId}`)
+      setMovie(response.data)
+    }
+    fetchData()
+  },[movieId])
+
+  if(!movie){
+    return null
+  }
+  
   return (
-    <div>
-      
-    </div>
+    <section>
+      <img className='modal__poster-img' src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt="img" />
+    </section>
   )
 }
