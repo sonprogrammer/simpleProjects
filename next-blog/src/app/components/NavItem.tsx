a2bbce94fec490231ce766b357de230e8db63d66
sonprogrammer <ods04138@gmail.com>
2024-03-21 19:25:38 +0900

    Feat : manage user function

--- next-blog/src/app/components/NavItem.tsx
+++ next-blog/src/app/components/NavItem.tsx
@@ -1,13 +1,19 @@
+import { signIn, signOut, useSession } from 'next-auth/react'
 import Link from 'next/link'
 import React from 'react'
 
 export default function NavItem({mobile} : {mobile?: boolean}) {
+  const {data : session, status} = useSession()
+  console.log({session}, status)
   return (
     <ul className={`text-md justify-center flex gap-4 w-full items-center ${mobile && "flex-col h-full"}`}>
       <li className='py-2 text-center border-b-4 cursor-pointer'><Link href='/admin'>Admin</Link></li>
       <li className='py-2 text-center border-b-4 cursor-pointer'><Link href='/user'>User</Link></li>
-      <li className='py-2 text-center border-b-4 cursor-pointer'><button>SignIn</button></li>
-      <li className='py-2 text-center border-b-4 cursor-pointer'><button>SignOut</button></li>
+      {session?.user ?
+        <li className='py-2 text-center border-b-4 cursor-pointer'><button onClick={()=> signOut()}>SignOut</button></li>
+        :
+        <li className='py-2 text-center border-b-4 cursor-pointer'><button onClick={()=>signIn()}>SignIn</button></li>
+    }
     </ul>
   )
 }
---
+++ next-blog/src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,49 @@
+import NextAuth, { NextAuthOptions } from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export const authOptions : NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!, // '!'를 넣으면 무조건 입력된다고 말해주는거임 undefined될 일 없다이말임
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+    }),
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "son's market",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+  
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null
+  
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      }
+    })
+  ],
+  session:{
+    strategy: 'jwt'
+  }
+  
+}
+
+export default NextAuth(authOptions)
