ef0095cca3ab64b9ec28a88421c616a52ff3ba9f
sonprogrammer <ods04138@gmail.com>
2024-03-21 17:15:05 +0900

    connect with orm

--- next-blog/src/app/components/NavItem.tsx
+++ next-blog/src/app/components/NavItem.tsx
@@ -5,7 +5,7 @@ export default function NavItem({mobile} : {mobile?: boolean}) {
   return (
     <ul className={`text-md justify-center flex gap-4 w-full items-center ${mobile && "flex-col h-full"}`}>
       <li className='py-2 text-center border-b-4 cursor-pointer'><Link href='/admin'>Admin</Link></li>
-      <li className='py-2 text-center border-b-4 cursor-pointer'><Link href='/admin'>User</Link></li>
+      <li className='py-2 text-center border-b-4 cursor-pointer'><Link href='/user'>User</Link></li>
       <li className='py-2 text-center border-b-4 cursor-pointer'><button>SignIn</button></li>
       <li className='py-2 text-center border-b-4 cursor-pointer'><button>SignOut</button></li>
     </ul>
