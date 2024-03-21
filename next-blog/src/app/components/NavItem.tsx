bd7817ffc6f6aa6a613b72ebab1f2f8db67c19a9
sonprogrammer <ods04138@gmail.com>
2024-03-21 16:47:41 +0900

    Feat : navbar

---
+++ next-blog/src/app/components/NavItem.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function NavItem({mobile} : {mobile?: boolean}) {
+  return (
+    <ul className={`text-md justify-center flex gap-4 w-full items-center ${mobile && "flex-col h-full"}`}>
+      <li className='py-2 text-center border-b-4 cursor-pointer'><Link href='/admin'>Admin</Link></li>
+      <li className='py-2 text-center border-b-4 cursor-pointer'><Link href='/admin'>User</Link></li>
+      <li className='py-2 text-center border-b-4 cursor-pointer'><button>SignIn</button></li>
+      <li className='py-2 text-center border-b-4 cursor-pointer'><button>SignOut</button></li>
+    </ul>
+  )
+}
