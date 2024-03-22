7b2d0fb7b3642f9c0633d465da1ba00fdf3ed2f3
sonprogrammer <ods04138@gmail.com>
2024-03-22 18:27:42 +0900

    Feat : UI

---
+++ next-blog/src/app/products/upload/page.tsx
@@ -0,0 +1,101 @@
+'use client'
+
+import Button from '@/app/components/Button'
+import Container from '@/app/components/Container'
+import Heading from '@/app/components/Heading'
+import Input from '@/app/components/Input'
+import React, { useState } from 'react'
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
+
+export default function ProductUploadPage() {
+  const [isLoading, setIsLoading] = useState(false)
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm<FieldValues>({
+    defaultValues: {
+      title: '',
+      description: '',
+      category: '',
+      latitude: 33.5563,
+      longtitude: 126.79581,
+      imageSrc: '',
+      price: 1,
+    },
+  })
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+
+  }
+
+  return (
+    <Container>
+      <div
+        className='
+            max-w-screen-lg
+            mx-auto
+        '>
+        <form
+            className='
+                flex
+                flex-col
+                gap-8
+            '
+            onSubmit={handleSubmit(onSubmit)}
+        >
+            <Heading 
+                title='Product Upload'
+                subtitle='upload your product'
+            />
+
+          <Input
+            id='title'
+            label='Title'
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+          />
+          <hr />
+          <Input
+            id='description'
+            label='Description'
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+          />
+          <hr />
+          <Input
+            id='price'
+            label='Price'
+            formatPrice
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+          />
+          <hr />
+
+          <div
+            className='
+            grid
+            grid-cols-1
+            md:grid-cols-2
+            gap-3
+            max-h-[50vh]
+            overflow-y-auto
+            '></div>
+          <hr />
+
+          <Button label='상품 생성하기' />
+        </form>
+      </div>
+    </Container>
+  )
+}
