6e77b4e07e7b4fa31513ff72cecc976ae6205715
sonprogrammer <ods04138@gmail.com>
2024-03-23 20:40:50 +0900

    Feat : UI

--- next-blog/src/app/products/upload/page.tsx
+++ next-blog/src/app/products/upload/page.tsx
@@ -53,6 +53,7 @@ export default function ProductUploadPage() {
                 subtitle='upload your product'
             />
 
+
           <Input
             id='title'
             label='Title'
