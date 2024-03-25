0dbbf5b2674d2b15e20a68c3d63d4c32eccc3c61
sonprogrammer <ods04138@gmail.com>
2024-03-25 19:52:03 +0900

    Feat : home page

--- next-blog/src/app/(home)/page.tsx
+++ next-blog/src/app/(home)/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 import getProducts, { ProductsParams } from "../actions/getProducts";
 import Container from "../components/Container";
 import EmptyState from "../components/EmptyState";
+import ProductCard from "../components/ProductCard";
+import getCurrentUser from "../actions/getCurrentUser";
+import FloatingButton from "../components/FloatingButton";
 
 interface HomeProps{
   searchParams: ProductsParams
@@ -10,6 +13,7 @@ interface HomeProps{
 export default async function Home({searchParams}: HomeProps) {
 
   const products = await getProducts(searchParams)
+  const currentUser = await getCurrentUser()
 
   console.log(products)
   
@@ -17,18 +21,37 @@ export default async function Home({searchParams}: HomeProps) {
    <Container>
     {/* category */}
 
-    <EmptyState />
-    {/* {
+    {
       products?.data.length === 0 
       ?
       <EmptyState />
       :
-      <>
-      <div>
-        hi
+      <> 
+      <div className="
+        grid
+        grid-cols-1
+        gap-8
+        pt-12
+        sm:grid-cols-2
+        md:gird-cols-3
+        lg:grid-cols-4
+        2xl:grid-cols-6
+         ">
+        {products.data.map((product) =>(
+          <ProductCard 
+            currentUser={currentUser}
+            key={product.id}
+            data={product}
+          />
+
+        ))}
       </div>
       </>
-    } */}
+    }
+
+    <FloatingButton 
+      href="/products/upload"
+    >+</FloatingButton>
    </Container>
   );
 }
