db83484b4b63c7cf5e9dbfbedf2a5624b65933a8
sonprogrammer <ods04138@gmail.com>
2024-03-26 15:29:22 +0900

    Feat : favorite function

---
+++ next-blog/src/app/api/favorites/[productId]/route.ts
@@ -0,0 +1,36 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import { NextResponse } from "next/server";
+
+interface Params{
+    productId?: string;
+}
+
+export async function POST(request: Request, {params}: {params: Params}){
+    const currentUser = await getCurrentUser();
+
+    if(!currentUser){
+        return NextResponse.error()
+    }
+
+    const { productId } = params
+
+    if(!productId || typeof productId !== 'string'){
+        throw new Error('invalid id')
+    }
+
+    let favoriteIds = [...(currentUser.favoriteIds || [])]
+
+    favoriteIds.push(productId)
+
+    const user = await prisma?.user.update({
+        where: {
+            id: currentUser.id
+        },
+        data:{
+            favoriteIds: favoriteIds
+        }
+    })
+
+    return NextResponse.json(user)
+    
+}
