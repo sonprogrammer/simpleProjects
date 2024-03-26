dcb3914d37cfd3080c4a033623c31667506ef6a2
sonprogrammer <ods04138@gmail.com>
2024-03-26 17:58:45 +0900

    Feat : loading function

---
+++ next-blog/src/app/components/Loader.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import React from 'react'
+import { RotatingLines } from 'react-loader-spinner'
+
+export default function Loader() {
+  return (
+    <div className='h-[60vh] max-w-6xl mx-auto w-full flex flex-row items-center justify-center py-40'>
+      <RotatingLines 
+        strokeColor='grey'
+        strokeWidth='5'
+        animationDuration='0.75'
+        width='30'
+        visible={true}
+      />
+    </div>
+  )
+}
---
+++ next-blog/src/app/user/loading.tsx
@@ -0,0 +1,8 @@
+import Loader from '@/app/components/Loader'
+import React from 'react'
+
+export default function Loading() {
+  return (
+    <Loader />
+  )
+}
