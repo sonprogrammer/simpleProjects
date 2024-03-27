f18aaa7e963ed113ef0fee9297b21e25e5f79d50
sonprogrammer <ods04138@gmail.com>
2024-03-27 15:59:30 +0900

    Feat : chat schema

--- next-blog/src/app/chat/ChatClient.tsx
+++ next-blog/src/app/chat/ChatClient.tsx
@@ -1,8 +1,10 @@
 'use client'
 
+import { TUserWithChat } from '@/types'
 import { User } from '@prisma/client'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import useSWR from 'swr'
 
 interface ChatClientProps{
     currentUser?: User | null 
@@ -19,10 +21,24 @@ export default function ChatClient({ currentUser}: ChatClientProps) {
     
     const [layout, setLayout] = useState(false)
 
-    useEffect(()=>{
-        axios.get(`/api/chat`)
-            .then((res) => console.log(res))
-    },[])
+    const fetcher = (url: string) => axios.get(url).then((res)=>res.data)
+    
+    const {data: users, error, isLoading } = useSWR('/api/chat', fetcher,{
+        refreshInterval: 1000
+    })
+    console.log(users)
+
+    users?.find((user: TUserWithChat)=> user.email === currentUser?.email)
+
+    if(isLoading) return <p>Loading...</p>
+    if(error){
+        return ( <p>Error!</p>)
+    }
+
+    // useEffect(()=>{
+    //     axios.get(`/api/chat`)
+    //         .then((res) => console.log(res))
+    // },[])
     
   return (
     <main>
