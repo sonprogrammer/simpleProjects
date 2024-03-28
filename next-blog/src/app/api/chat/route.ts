d12ca138096116eaa354a36a0da5bff29a7c1ffc
sonprogrammer <ods04138@gmail.com>
2024-03-28 15:58:09 +0900

    Feat : chat page

--- next-blog/src/app/api/chat/route.ts
+++ next-blog/src/app/api/chat/route.ts
@@ -31,3 +31,86 @@ export async function GET(
     })
     return NextResponse.json(users)
 }
+
+
+export async function POST(
+    request: Request
+){
+    const currentUser = await getCurrentUser()
+
+    if(!currentUser){
+        return NextResponse.error()
+    }
+    const body = await request.json()
+
+    const conversation = await prisma.conversation.findFirst({
+        where:{
+            AND:[
+                {
+                    users: {
+                        some:{
+                            id: body.senderId
+                        }
+                    }
+                },
+                {
+                    users:{
+                        some:{
+                            id:body.receiverId
+                        }
+                    }
+                }
+            ]
+        }
+    })
+    if(conversation){
+        try {
+            const message = await prisma.message.create({
+                data:{
+                    text: body.text,
+                    image: body.image,
+                    senderId: body.senderId,
+                    receiverId: body.receiverId,
+                    conversationId: conversation.id
+                }
+            })
+
+            return NextResponse.json(message)
+        } catch (error) {
+            return NextResponse.json(error)
+        }
+    }else{
+        const newConversation = await prisma.conversation.create({
+            data:{
+                senderId: body.senderId,
+                receiverId: body.receiverId,
+                users:{
+                    connect:[
+                        {
+                            id: body.senderId,
+                        },
+                        {
+                            id: body.receiverId,
+                        }
+                    ]
+                }
+            }
+        })
+        try {
+            const message = await prisma.message.create({
+                data:{
+                    text: body.text,
+                    image: body.image,
+                    senderId: body.senderId,
+                    receiverId: body.receiverId,
+                    conversationId: newConversation.id
+                }
+            })
+
+            return NextResponse.json(message)
+        } catch (error) {
+            return NextResponse.json(error)
+        }
+    }
+}
+
