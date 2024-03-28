9d4aac1f592a9aa62f845dc9d06af7d915997e7d
sonprogrammer <ods04138@gmail.com>
2024-03-28 16:20:57 +0900

    Feat : chat page

--- next-blog/src/app/components/chat/Chat.tsx
+++ next-blog/src/app/components/chat/Chat.tsx
@@ -1,6 +1,7 @@
 import { TUserWithChat } from '@/types'
 import React from 'react'
 import Input from './Input'
+import ChatHeader from './ChatHeader'
 
 interface ChatProps{
   currentUser: TUserWithChat
@@ -21,10 +22,23 @@ export default function Chat({
   if(!receiver.receiverName || !currentUser){
     return <div className='h-full w-full'></div>
   }
+
+  const conversation = currentUser?.conversations.find((conversation) =>
+    conversation.users.find((user)=> user.id === receiver.receiverId)
+  )
   return (
     <div className='w-full'>
       <div>
-
+        <ChatHeader 
+          setLayout={setLayout}
+          receiverName={receiver.receiverName}
+          receiverImage={receiver.receiverImage}
+          lastMessageTime={
+            conversation?.messages
+            .filter(message => message.receiverId === currentUser.id)
+            .slice(-1)[0]?.createdAt
+          }
+        />
       </div>
       <div className='flex flex-col gap-8 p-4 overflow-hidden h-[calc(100vh_-_60px_-_70px_-_80px)]'>
         
--- next-blog/src/app/components/chat/User.tsx
+++ next-blog/src/app/components/chat/User.tsx
@@ -4,7 +4,7 @@ import Avatar from '../Avatar'
 import { fromNow } from '@/helpers/dayjs'
 
 
-interface UserProps{
+interface UserProps {
   user: TUserWithChat
   currentUserId: string
 }
@@ -16,32 +16,32 @@ export default function User({
 }: UserProps) {
 
   const messageWithCurrentUser = user.conversations.find(
-    (conversation: TConversation)=>
-      conversation.users.find((user)=> user.id === currentUserId)
+    (conversation: TConversation) =>
+      conversation.users.find((user) => user.id === currentUserId)
   )
-  
+
   const latestMessage = messageWithCurrentUser?.messages.slice(-1)[0]
-  
+
   return (
     <div className='grid grid-cols-[40px_1fr_50px] grid-rows-[40px] gap-3 py-3 px-4 border-b-[1px] hover:cursor-pointer hover:bg-orange-500'>
       <div className=''>
-        <Avatar src={user.image}/>
+        <Avatar src={user.image} />
       </div>
       <div>
         <h3>{user.name}</h3>
         {latestMessage && <p className='overflow-hidden text-xs font-medium text-gray-600 break-words whitespace-pre-wrap'>
           {latestMessage.text}
-          </p>}
-          {latestMessage && latestMessage.image && 
+        </p>}
+        {latestMessage && latestMessage.image &&
           <p className='text-xs font-medium text-gray-600'>[이미지]</p>}
 
-          <div>
-            {latestMessage && (
-              <p>
-                {fromNow(latestMessage.createdAt)}
-              </p>
-            )}
-          </div>
+      </div>
+      <div className='flex justify-end text-xs text-gray-500'>
+        {latestMessage && (
+          <p>
+            {fromNow(latestMessage.createdAt)}
+          </p>
+        )}
       </div>
     </div>
   )
