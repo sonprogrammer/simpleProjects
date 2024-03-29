aa91134e6430c99adc739e3b6dbe8f04f5c70eaa
sonprogrammer <ods04138@gmail.com>
2024-03-29 20:17:31 +0900

    components

--- youtube/src/App.jsx
+++ youtube/src/App.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import { Outlet, Route, Routes } from 'react-router-dom'
+import MainPage from './pages/MainPage'
+import SearchedVideoPage from './pages/SearchedVideoPage'
+import VideoPage from './pages/VideoPage'
+import NavigationBar from './components/NavigationBar'
+import SideBar from './components/SideBar'
+
+const Layout = () =>{
+  return(
+    <>
+        <NavigationBar />
+        <SideBar />
+      <main>
+        <Outlet />
+      </main>
+    </>
+  )
+}
 
 
 function App() {
 
   return (
     <div>
-      
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<MainPage />}/>
+          <Route path='/results/:input' element={<SearchedVideoPage />} />
+          <Route path='/video/:videoId' element={<VideoPage />} />
+        </Route>
+      </Routes>
     </div>
   )
 }
