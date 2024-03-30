ab4001eeb7f01828d3ee3f4d8bfc6b105ce80d7b
sonprogrammer <ods04138@gmail.com>
2024-03-30 17:33:55 +0900

    Feat: sidebar

--- youtube/src/components/SideBar/SmallSideBar.jsx
+++ youtube/src/components/SideBar/SmallSideBar.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { AiFillHome } from 'react-icons/ai'
+import { FaCompass } from 'react-icons/fa'
+import { MdSubscriptions } from 'react-icons/md'
+import { Link } from 'react-router-dom'
 
 export default function SmallSideBar() {
   return (
-    <div style={{color: 'white', marginTop: 200, fontSize: 200}}>
-      hi
-    </div>
+    <aside className='small_sidebar'>
+      <div className='small_sidebar_container'>
+        <Link to='/' data-for='sidebar' data-tip='Home'>
+          <div className='sidebar_icon_container'>
+            <AiFillHome size={23} className='sidebar_icon' />
+            <h4 className='sidebar_text'>Home</h4>
+          </div>
+        </Link>
+        <Link to='/feed/explore' data-for='sidebar' data-tip='Home'>
+          <div className='sidebar_icon_container'>
+            <FaCompass size={23} className='sidebar_icon' />
+            <h4 className='sidebar_text'>Explore</h4>
+          </div>
+        </Link>
+        <Link to='/' data-for='sidebar' data-tip='Home'>
+          <div className='sidebar_icon_container'>
+            <MdSubscriptions size={23} className='sidebar_icon' />
+            <h4 className='sidebar_text'>Subscriptions</h4>
+          </div>
+        </Link>
+      </div>
+    </aside>
   )
 }
