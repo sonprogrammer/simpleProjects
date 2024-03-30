245e56d615a51f3a3310940a5cb8f12ff6d51480
sonprogrammer <ods04138@gmail.com>
2024-03-30 15:44:45 +0900

    Fea: hooks for responsive web

--- youtube/src/components/NavigationBar/SearchBar/index.jsx
+++ youtube/src/components/NavigationBar/SearchBar/index.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import {ImSearch} from 'react-icons/im'
 import {MdKeyboardVoice} from 'react-icons/md'
+import useWindowSize from '../../../helpers/useWindowSize'
 
 export default function SearchBar() {
+  const { width } = useWindowSize()
   return (
-    <div className='SearchBar'>
-      <form>
+    <div className={`SearchBar ${width <= 640 ? 'smallSearch' : ''}`}>
+      {
+        width > 640 ? 
+
+        (<form>
         <input type="text" name='search' placeholder='Search' />
         <button type='submit'>
           <ImSearch size={20}/>
         </button>
-      </form>
-
+      </form>)
+      : 
+      <button className='icon-container searchIcon'>
+        <ImSearch size={20}/>
+      </button>
+}
     <button className='icon-container voiceIcon'>
       <MdKeyboardVoice size={25}/>
     </button>
