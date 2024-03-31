7d1204b721dfa099a37393f6f7e519250acdc2dd
sonprogrammer <ods04138@gmail.com>
2024-03-31 21:45:25 +0900

    Feat: sidebar

--- youtube/src/main.jsx
+++ youtube/src/main.jsx
@@ -17,5 +17,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         </SidebarContextProvider>
       </SearchContextProvider>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
