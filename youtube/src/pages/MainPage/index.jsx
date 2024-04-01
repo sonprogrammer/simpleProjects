81fd9cb989dfc4207cfd3dd172499f9133737bd3
sonprogrammer <ods04138@gmail.com>
2024-04-01 14:46:12 +0900

    Feat : axios instance

--- youtube/src/pages/MainPage/index.jsx
+++ youtube/src/pages/MainPage/index.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+import axios from '../../api/axios'
+import { getVideoInfo } from '../../helpers/fetchingData'
 
 export default function MainPage() {
+  const [mainVideos, setMainVideos] = useState([])
+
+  const getMainVideos = useCallback(async () =>{
+    try {
+      const response = await axios.get(`/search?part=snippet&maxResults=10&q=beautiful%20place`)
+      let videosArray = response.data.items;
+      videosArray = await getVideoInfo(videosArray)
+      console.log('videoarra',videosArray)
+    } catch (error) {
+      console.log(error)
+    }
+  },[])
+  
+  useEffect(()=>{
+    getMainVideos()
+  },[getMainVideos])
+
   return (
     <div>
-      
+      mainpage      
     </div>
   )
 }
