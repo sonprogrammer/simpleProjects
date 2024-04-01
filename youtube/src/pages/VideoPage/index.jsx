a22b1cdcfebc546074908d0a47cd40f281712c5e
sonprogrammer <ods04138@gmail.com>
2024-04-01 17:59:25 +0900

    Feat: video page

--- youtube/src/pages/VideoPage/index.jsx
+++ youtube/src/pages/VideoPage/index.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { useLocation, useParams } from 'react-router-dom'
+import YouTube from 'react-youtube'
 
 export default function VideoPage() {
+  const {videoId} = useParams()
+  let location = useLocation()
+  const { state: currentVideo} = location
+  const onPlayerReady = (e) =>{
+    e.target.playVideo()
+  }
+
+  const opts ={
+    width: '640',
+    height: '390',
+    playerVars:{
+      autoplay: 1
+    }
+  }
   return (
     <div>
-      
+      <YouTube className='youtube-player' videoId={videoId} onPlay={onPlayerReady} opts={opts}/>
     </div>
   )
 }
