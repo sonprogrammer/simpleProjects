256f6de2323c9fef3262e0b458b2f314eb5d7197
sonprogrammer <ods04138@gmail.com>
2024-04-03 18:20:43 +0900

    Feat: search page

--- youtube/src/pages/SearchedVideoPage/index.jsx
+++ youtube/src/pages/SearchedVideoPage/index.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { SearchContext } from '../../context/SearchContext'
+import { getVideoInfo } from '../../helpers/fetchingData'
+import Videocard from '../../components/VideoCard/'
+import { SidebarContext } from '../../context/SidebarContext'
+
 
 export default function SearchedViedoPage() {
+
+  const [searchedVideos, setSearchedVideos] = useState([])
+  const { searchQuery } = useContext(SearchContext)
+  const { setIsToggled } = useContext(SidebarContext)
+
+  const loadVideoInfo = async () => {
+    setIsToggled(true)
+    const videos = await getVideoInfo(searchQuery.videos)
+    setSearchedVideos(videos)
+  }
+
+  useEffect(() => {
+    loadVideoInfo
+  }, [loadVideoInfo])
+
+
+  const searchedVideosMarkup = searchedVideos?.map(video => (
+    <Videocard 
+      key={video.id.videoId}
+      id={video.id.videoId}
+      video={video}
+      img={video.snippet.thumbnails.medium.url}
+      info={video.snippet}
+      eInfo={video.extraInfo}
+      channelInfo={video.channelInfo}
+    />
+  ))
+  
   return (
-    <div>
-      
-    </div>
+    <section className=''>
+      {searchedVideosMarkup}
+    </section>
   )
 }
