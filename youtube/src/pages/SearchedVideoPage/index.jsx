2e9779dc1f559dcfb8655f552287b6bdc5be0756
sonprogrammer <ods04138@gmail.com>
2024-04-03 18:22:12 +0900

    Feat: search page

--- youtube/src/pages/SearchedVideoPage/index.jsx
+++ youtube/src/pages/SearchedVideoPage/index.jsx
@@ -35,7 +35,7 @@ export default function SearchedViedoPage() {
   ))
   
   return (
-    <section className=''>
+    <section className='searchedVideos'>
       {searchedVideosMarkup}
     </section>
   )
